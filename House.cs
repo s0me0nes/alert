af240687706e57b8b3952728349f65e551d254da
s0me0nes <93438449+s0me0nes@users.noreply.github.com>
2023-08-28 14:40:52 +0400

    Add files via upload

---
+++ House.cs
@@ -0,0 +1,28 @@
+using UnityEngine;
+
+[RequireComponent(typeof(Alarm))]
+public class House : MonoBehaviour
+{
+    private Alarm _alarm;
+
+    private void Awake()
+    {
+        _alarm = GetComponent<Alarm>();
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.TryGetComponent(out Thief thief))
+        {
+            _alarm.Activation();
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.gameObject.TryGetComponent(out Thief thief))
+        {
+            _alarm.Deactivation();
+        }
+    }
+}
