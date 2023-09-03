8f5fe43f8f47250b46864a8c00e9d06c1b6a4e0e
s0me0nes <93438449+s0me0nes@users.noreply.github.com>
2023-09-03 17:25:10 +0400

    Update Thief.cs

--- Thief.cs
+++ Thief.cs
@@ -5,6 +5,7 @@ public class Thief : MonoBehaviour
     [SerializeField] private Transform _firstPoint, _finishPoint;
     [SerializeField] private float _speed;
     [SerializeField] private GameObject _door;
+    [SerializeField] private float _waitingTime = 6f;
 
     private bool _isGrab;
 
@@ -12,30 +13,32 @@ public class Thief : MonoBehaviour
     {
         if (_isGrab == false)
         {
-            transform.position = 
-                Vector2.MoveTowards(transform.position, _finishPoint.transform.position, _speed * Time.deltaTime);
+            Move(_finishPoint);
         }
         else if (_isGrab)
         {
-            transform.position = 
-                Vector2.MoveTowards(transform.position, _firstPoint.transform.position, _speed * Time.deltaTime);
+            Move(_firstPoint);
         }
     }
 
-    private void Comeback()
+    private void ComeBack()
     {
         _isGrab = true;
         _door.SetActive(false);
     }
 
+    private void Move(Transform target)
+    {
+        transform.position =
+                Vector2.MoveTowards(transform.position, target.transform.position, _speed * Time.deltaTime);
+    }
+
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        float waitingTime = 6f;
-
         if (collision.gameObject.TryGetComponent<Alarm>(out Alarm alarm))
         {
             _door.SetActive(true);
-            Invoke(nameof(Comeback), waitingTime);
+            Invoke(nameof(ComeBack), _waitingTime);
         }
     }
 }
