ec0ff50058e7831e66302f1a054f7895740ecb04
s0me0nes <93438449+s0me0nes@users.noreply.github.com>
2023-09-04 14:04:30 +0400

    Update Alarm.cs

--- Alarm.cs
+++ Alarm.cs
@@ -4,7 +4,8 @@ using UnityEngine;
 [RequireComponent(typeof(AudioSource))]
 public class Alarm : MonoBehaviour
 {
-    [SerializeField] private float _volumeChangeSpeed = 1;
+    [SerializeField] private float _volumeSpeed = 1;
+    [SerializeField] private float _runningSpeed = 0.05f;
 
     private AudioSource _alert;
     private Coroutine _changeVolumeCoroutine;
@@ -22,6 +23,7 @@ public class Alarm : MonoBehaviour
     {
         if (_changeVolumeCoroutine != null)
         {
+            _alert.Play();
             StopCoroutine(_changeVolumeCoroutine);
         }
 
@@ -30,19 +32,26 @@ public class Alarm : MonoBehaviour
 
     public void Deactivate()
     {
+        int stopPlayback = 5;
+
         StopCoroutine(_changeVolumeCoroutine);
         _changeVolumeCoroutine = StartCoroutine(ChangeVolume(_volumeOff));
+        Invoke(nameof(VolumeOff), stopPlayback);
+    }
+
+    private void VolumeOff()
+    {
+        _alert.Stop();
     }
 
     private IEnumerator ChangeVolume(float targetValue)
     {
-        _alert.Play();
-        var _volumeSpeed = new WaitForSeconds(0.05f);
+        var updateFrequency = new WaitForSeconds(_runningSpeed);
 
         while (_alert.volume != targetValue)
         {
-            _alert.volume = Mathf.MoveTowards(_alert.volume, targetValue, _volumeChangeSpeed * Time.deltaTime);
-            yield return _volumeSpeed;
+            _alert.volume = Mathf.MoveTowards(_alert.volume, targetValue, _volumeSpeed * Time.deltaTime);
+            yield return updateFrequency;
         }
     }
 }
